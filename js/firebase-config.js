b34bc900e571cd95d8e646ed2e39b3d97d8f1bcd
JLMSTW <jlmandarinschool@gmail.com>
2025-05-12 20:31:11 +0800

    Add firebase-config.js (範例設定)

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+
+export const firebaseConfig = {
+  apiKey: "AIza...YourKeyHere",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdefg1234567"
+};
