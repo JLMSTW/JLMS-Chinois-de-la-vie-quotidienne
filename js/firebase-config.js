f62400a2fb7b9d02149eac10db1f4586338844f5
JLMSTW <jlmandarinschool@gmail.com>
2025-05-14 09:39:53 +0800

    修正 firebase-config.js：更新正確的 API 金鑰

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,17 @@
-// js/firebase-config.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export const firebaseConfig = {
+// Your web app's Firebase configuration
+const firebaseConfig = {
   apiKey: "AIzaSyBBHF_cujKBqLqekZX2CRq4Ue7Mdzp7fy0",
   authDomain: "jlms-login.firebaseapp.com",
   projectId: "jlms-login",
-  storageBucket: "jlms-login.appspot.com",
+  storageBucket: "jlms-login.firebasestorage.app",
   messagingSenderId: "309559732738",
   appId: "1:309559732738:web:180c1d7f9a54b34c9a951b"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
