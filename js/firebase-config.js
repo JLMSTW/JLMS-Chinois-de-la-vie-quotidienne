a8c49e0a8851f7fe3e8e5ccf4a465150c5a0e4fd
JLMSTW <jlmandarinschool@gmail.com>
2025-05-29 10:40:00 +0800

    fix: 將 firebase-config.js 改為 export 格式以供其他模組引入

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBBHF_cujKBqLqekZX2CRq4Ue7Mdzp7fy0",
   authDomain: "jlms-login.firebaseapp.com",
   projectId: "jlms-login",
@@ -12,6 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "309559732738",
   appId: "1:309559732738:web:180c1d7f9a54b34c9a951b"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
