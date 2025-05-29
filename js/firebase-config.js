720dd32104521657e36c38af4ee1b50225748a15
JLMSTW <jlmandarinschool@gmail.com>
2025-05-29 10:45:17 +0800

    fix: storageBucket: "jlms-login.appspot.com"

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyBBHF_cujKBqLqekZX2CRq4Ue7Mdzp7fy0",
   authDomain: "jlms-login.firebaseapp.com",
   projectId: "jlms-login",
-  storageBucket: "jlms-login.firebasestorage.app",
+  storageBucket: "jlms-login.appspot.com",
   messagingSenderId: "309559732738",
   appId: "1:309559732738:web:180c1d7f9a54b34c9a951b"
 };
